33fac57097d159106c864ed521c5c7eda292621e
pryvalovbogdan <pryvalov.bogddan@gmail.com>
2019-08-21 02:07:10 +0300

    proj1

---
+++ proj.js
@@ -0,0 +1,116 @@
+
+/*Класс blinds(жалюзи)
+   состояние
+      _status: Boolean
+      _sunLight: Number
+      
+   поведение
+      getStatus(): String
+      open(): void
+      close(): void
+
+      moreSunLight(): void
+      lessSunLight(): void
+      setSunLight(level: Number): void
+      getSunLight(): Number
+      
+*/
+
+function Blinds(sunLight){
+	this._status = false;
+	this._sunLight = sunLight;
+	}
+Blinds.prototype.open = function() {
+	this._status = true;
+}
+
+Blinds.prototype.close = function() {
+	this._status = false;
+}
+Blinds.prototype.getStatus = function() {
+	return this._status ? "Opened Blinds" : "Closed Blinds";
+}
+
+Blinds.prototype.moreSunLight = function(sunLight){
+	if(this._sunLight < 95 ) {
+	this._sunLight += 5;
+} 
+}
+Blinds.prototype.lessSunLight = function(sunLight){
+	if(this._sunLight > 5) {
+	this._sunLight -= 5;
+} 
+}
+Blinds.prototype.setSunLight = function(sunLight){
+	if(!isNaN(sunLight)&& typeof sunLight == "number" && sunLight >= 0 && sunLight <= 100){
+	this._sunLight = sunLight;
+	} 
+}
+
+Blinds.prototype.getSunLight = function(){
+	return this._sunLight + "%";
+}
+var f = new Blinds(99);
+
+/*Класс airConditioner(states)
+   состояние
+      _status: Boolean
+      _states: ["String"];
+      currentState: Number;
+      
+   поведение
+      getStatus(): String
+      on(): void
+      off(): void
+
+      coldState(): void
+      warmState(): void
+      turboState(): void
+      nextState(): void
+      previousState(): void
+      getState()
+*/
+
+
+
+class AirConditioner {
+		constructor() {
+			this._mode = ["coldMode", "warmMode", "turboMode"];
+			this._status = false;
+			this._currentMode = 0;
+		}
+		on(){
+			this._status = true;
+		}
+		off(){
+			this._status = false;
+		}
+
+		get status(){
+			return this._status ? "On" : "Off";
+		}
+		nextMode(){
+		   	this._currentMode++;
+		}
+		previousMode(){	
+			this._currentMode--;
+		}
+		
+		coldMode(){
+			this._currentMode = 0;
+		}
+		warmMode(){
+			this._currentMode = 1;
+		}
+		turboMode(){
+			this._currentMode = 2;
+		}
+		
+		get modes(){
+			return this._mode[this._currentMode];
+		}
+}
+
+let p = new AirConditioner()
+
+
